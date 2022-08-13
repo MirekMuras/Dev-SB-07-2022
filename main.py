bd750527bd23cd84ef61bd2d596842acf4f3d391
Miro <mmuras@boostr.com>
2022-08-13 12:12:00 -0400

    new_commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import test1
+
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -7,10 +9,13 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    df = test1.dummy_data()
+    print(df.columns)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hi this is Miro!')
+    print_hi('this is Miro and this is my testing environment!\n'
+             '--------------------------------------------------')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ test1.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+import subprocess
+
+
+from faker import Faker
+
+
+def testing():
+    try:
+        with open (os.devnull,"w") as devnull:
+            subprocess.check_output(devnull, shell=True, stderr=devnull)
+            return False
+    except subprocess.CalledProcessError:
+        return True
+
+#to create and generate a fake generator use the Faker() method
+def dummy_data():
+    generate_fake_profile = Faker()
+    #genarate fake 10000 profile
+    data = [generate_fake_profile.profile() for i in range(10000)]
+    #cretae fake profile dataframe
+    df = pd.DataFrame(data=data)
+
+    return df
+
